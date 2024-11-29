e9ef5209d29b85c1d7959bfbf4ffc2ef853b2c2f
BernardoTaciano <bernardotaciano1@gmail.com>
2024-11-29 10:06:45 +0000

    Second Blood. Added Bottom Bar Navigation.

--- app/src/main/java/com/example/summonerscompass/MainActivity.kt
+++ app/src/main/java/com/example/summonerscompass/MainActivity.kt
@@ -1,32 +1,108 @@
 package com.example.summonerscompass
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.summonerscompass.ui.theme.SummonersCompassTheme
 
+
+data class  BottomNavigationItem(
+    val title : String,
+    val selectedIcon : ImageVector,
+    val unselectedIcon : ImageVector,
+)
+
+
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SummonersCompassTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+        ),
+        BottomNavigationItem(
+            title = "Glossary",
+            selectedIcon = Icons.Filled.Menu,
+            unselectedIcon = Icons.Outlined.Menu,
+        ),
+        BottomNavigationItem(
+            title = "Crafting",
+            selectedIcon = Icons.Filled.Add,
+            unselectedIcon = Icons.Outlined.Add,
+        )
+    )
+
+    var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = selectedItemIndex == index,
+                        onClick = { selectedItemIndex = (index) },
+                        label = { Text(text = item.title) },
+                        icon = {
+                            Icon(
+                                imageVector = if (selectedItemIndex == index) item.selectedIcon else item.unselectedIcon,
+                                contentDescription = item.title
+                            )
+                        }
                     )
                 }
             }
         }
+    ) { innerPadding ->
+        ContentScreen(selectedItemIndex, Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun ContentScreen(selectedIndex: Int, modifier: Modifier = Modifier) {
+    when (selectedIndex) {
+        0 -> Greeting(name = "Home", modifier = modifier)
+        1 -> Greeting(name = "Glossary", modifier = modifier)
+        2 -> Greeting(name = "Crafting", modifier = modifier)
     }
 }
 
